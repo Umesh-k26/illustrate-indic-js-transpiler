18f614d8a0307ea6e25b524836933cbcb3576222
Umesh Kalvakuntla <umeshkalvakuntla26@gmail.com>
2022-11-04 16:35:27 +0530

    Added lexer for hindi language

--- src/index.js
+++ src/index.js
@@ -3,14 +3,10 @@ const parser = require('./examples/jscore.js')
 
 
 fs.readFile('test.js', (err, data) => {
-  console.log(data);
   data = data.toString('utf-8')
   console.log(data);
   res = parser.parse(data);
 
-  // end = res[res.length-1]
-  // console.log(end[1])
-  // console.log(res[0][1]);
   console.log(res);
   console.log(JSON.stringify(res));
 
